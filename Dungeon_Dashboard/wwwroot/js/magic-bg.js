a50e948757576803c593e0e93a319bce6b683ba8
FilipChyla <f.chyla12@gmail.com>
2025-11-19 19:27:15 +0100

    Adjust firefly amount dynamically based on canvas size and improve flicker effect.

--- Dungeon_Dashboard/wwwroot/js/magic-bg.js
+++ Dungeon_Dashboard/wwwroot/js/magic-bg.js
@@ -1,12 +1,15 @@
 ﻿const canvas = document.getElementById('fireflies');
 const ctx = canvas.getContext('2d');
 let w, h, fireflies = [];
+let firefliesAmount;
 
 function resize() {
     w = canvas.width = canvas.offsetWidth;
     h = canvas.height = canvas.offsetHeight;
+    firefliesAmount = (w*h)/35000;
+    
 
-    fireflies = Array.from({ length: 40 }, () => ({
+    fireflies = Array.from({ length: Math.floor(firefliesAmount) }, () => ({
         x: Math.random() * w,
         y: Math.random() * h,
         r: Math.random() * 2 + 1,
@@ -24,7 +27,7 @@ function draw() {
         if (f.x < 0 || f.x > w) f.dx *= -1;
         if (f.y < 0 || f.y > h) f.dy *= -1;
         f.flicker += 0.03;
-        const alpha = 0.5 + Math.sin(f.flicker) * 0.5;
+        const alpha = 0.5 + Math.sin(f.flicker) * 0.7;
         ctx.beginPath();
         ctx.arc(f.x, f.y, f.r, 0, Math.PI * 2);
         ctx.fillStyle = `rgba(255, 255, 180, ${alpha})`;
